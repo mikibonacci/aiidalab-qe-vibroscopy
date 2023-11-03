0424a97d8eff72bf98b938a70f0c48299bc23205
Miki Bonacci <mikibonacci@hotmail.it>
2023-11-03 15:19:41 +0000

    adding the phonopy code

--- aiidalab_qe_vibroscopy/workflows/__init__.py
+++ aiidalab_qe_vibroscopy/workflows/__init__.py
@@ -3,13 +3,22 @@ from aiidalab_qe_vibroscopy.workflows.workchain import workchain_and_builder
 from aiidalab_qe_vibroscopy.workflows.result import Result
 from aiidalab_qe.common.panel import OutlinePanel
 
+from aiidalab_widgets_base import ComputationalResourcesWidget
+
 
 class Outline(OutlinePanel):
     title = "Vibrational properties"
     #description = "IR and Raman spectra; you may also select phononic and dielectric properties"
+    
+
+phonopy_code = ComputationalResourcesWidget(
+    description="phonopy",
+    default_calc_job_plugin="phonopy.phonopy",
+)
 
 property ={
 "outline": Outline,
+"code": {"phonopy": phonopy_code},
 "setting": Setting,
 "workchain": workchain_and_builder,
 "result": Result,
