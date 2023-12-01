6aa7e25aa1862df9ab36632c7efc1df3222df68b
Miki Bonacci <mikibonacci@hotmail.it>
2023-12-01 08:24:47 +0000

    also IR supported in raman

--- aiidalab_qe_vibroscopy/utils/raman/result.py
+++ aiidalab_qe_vibroscopy/utils/raman/result.py
@@ -18,21 +18,24 @@ from ase import Atoms
 
 
 from aiida_vibroscopy.utils.broadenings import multilorentz
+
+
 def plot_powder(
     frequencies: list[float],
     intensities: list[float],
     broadening: float = 10.0,
-    x_range: list[float] | str = 'auto',
+    x_range: list[float] | str = "auto",
     broadening_function=multilorentz,
-    normalize: bool = True,):
-    
+    normalize: bool = True,
+):
+
     frequencies = np.array(frequencies)
     intensities = np.array(intensities)
 
-    if x_range == 'auto':
+    if x_range == "auto":
         xi = max(0, frequencies.min() - 200)
         xf = frequencies.max() + 200
-        x_range = np.arange(xi, xf, 1.)
+        x_range = np.arange(xi, xf, 1.0)
 
     y_range = broadening_function(x_range, frequencies, intensities, broadening)
 
@@ -40,21 +43,20 @@ def plot_powder(
         y_range /= y_range.max()
 
     return x_range, y_range
-    
-    
+
+
 def export_iramanworkchain_data(node):
 
-    '''
+    """
     We have multiple choices: IR, RAMAN.
-    '''
-
+    """
 
     import json
 
     from monty.json import jsanitize
 
-    parameters={}
-    
+    parameters = {}
+
     if not "vibronic" in node.outputs:
         return None
     else:
@@ -62,27 +64,41 @@ def export_iramanworkchain_data(node):
             return None
 
     if "vibrational_data" in node.outputs.vibronic.iraman:
-        
+
         vibro = node.outputs.vibronic.iraman.vibrational_data.numerical_accuracy_4
-        
-        try:
-            #if node.inputs.iraman.dielectric.property == "ir":
-            polarized_intensities, frequencies, labels = vibro.run_powder_ir_intensities(frequency_laser=532, temperature=300)
-            total_intensities =  polarized_intensities 
+
+        if "raman_tensors" not in vibro.get_arraynames():
+            (
+                polarized_intensities,
+                frequencies,
+                labels,
+            ) = vibro.run_powder_ir_intensities()
+            total_intensities = polarized_intensities
+
             frequencies, total_intensities = plot_powder(frequencies, total_intensities)
 
             return [
-                total_intensities,frequencies,labels,'Infrared vibrational spectrum'
+                total_intensities,
+                frequencies,
+                labels,
+                "Infrared vibrational spectrum",
             ]
-        except:
-            #elif node.inputs.iraman.dielectric.property == "raman":
-            polarized_intensities, depolarized_intensities, frequencies, labels = vibro.run_powder_raman_intensities(frequency_laser=532, temperature=300)
-            total_intensities =  polarized_intensities + depolarized_intensities
+        else:
+            (
+                polarized_intensities,
+                depolarized_intensities,
+                frequencies,
+                labels,
+            ) = vibro.run_powder_raman_intensities(frequency_laser=532, temperature=300)
+            total_intensities = polarized_intensities + depolarized_intensities
             frequencies, total_intensities = plot_powder(frequencies, total_intensities)
             return [
-                total_intensities,frequencies,labels,'Raman vibrational spectrum'
+                total_intensities,
+                frequencies,
+                labels,
+                "Raman vibrational spectrum",
             ]
-  
+
     else:
         return None
 
@@ -95,12 +111,15 @@ class Result(ResultPanel):
     def _update_view(self):
         bands_data = export_iramanworkchain_data(self.node)
 
-        if bands_data[3] in ["Raman vibrational spectrum","Infrared vibrational spectrum"]:
+        if bands_data[3] in [
+            "Raman vibrational spectrum",
+            "Infrared vibrational spectrum",
+        ]:
             import plotly.graph_objects as go
 
             frequencies = bands_data[1]
             total_intensities = bands_data[0]
-            
+
             g = go.FigureWidget(
                 layout=go.Layout(
                     title=dict(text=bands_data[3]),
@@ -109,26 +128,34 @@ class Result(ResultPanel):
             )
             g.layout.xaxis.title = "Wavenumber (cm-1)"
             g.layout.yaxis.title = "Intensity (arb. units)"
-            g.layout.xaxis.nticks = 0 
-            g.add_scatter(x=frequencies,y=total_intensities,name=f"")
+            g.layout.xaxis.nticks = 0
+            g.add_scatter(x=frequencies, y=total_intensities, name=f"")
 
-            
             self.children = [
                 g,
-                ]
+            ]
+
 
 class SpectrumPlotWidget(ipw.VBox):
-    """Widget that allows different options for plotting Raman Spectrum."""
+    """Widget that allows different options for plotting Raman or IR Spectrum."""
 
-    description = ipw.HTML(
-        """<div style="line-height: 140%; padding-top: 10px; padding-bottom: 10px">
-        Select the type of Raman spectrum to plot.
-        </div>"""
-    )
     def __init__(self, node, **kwargs):
         self.node = node
-        #VibriationalData 
-        self.vibro = self.node.outputs.vibronic.iraman.vibrational_data.numerical_accuracy_4
+        vibro = self.node.outputs.vibronic.iraman.vibrational_data.numerical_accuracy_4
+
+        self.spectrum_type = (
+            "Raman" if "raman_tensors" in vibro.get_arraynames() else "IR"
+        )
+
+        self.description = ipw.HTML(
+            f"""<div style="line-height: 140%; padding-top: 10px; padding-bottom: 10px">
+            Select the type of {self.spectrum_type} spectrum to plot.
+            </div>"""
+        )
+        # VibrationalData
+        self.vibro = (
+            self.node.outputs.vibronic.iraman.vibrational_data.numerical_accuracy_4
+        )
         self._plot_type = ipw.ToggleButtons(
             options=[
                 ("Powder", "powder"),
@@ -185,14 +212,21 @@ class SpectrumPlotWidget(ipw.VBox):
         self.frequencies = []
         self.intensities = []
         self.polarization_out = ipw.Output()
-        
+
         def download_data(_=None):
             filename = "spectra.json"
-            my_dict = {"Frequencies cm-1": self.frequencies.tolist(), "Intensities": self.intensities.tolist()}
+            my_dict = {
+                "Frequencies cm-1": self.frequencies.tolist(),
+                "Intensities": self.intensities.tolist(),
+            }
             json_str = json.dumps(my_dict)
             b64_str = base64.b64encode(json_str.encode()).decode()
             self._download(payload=b64_str, filename=filename)
 
+        # hide the frequency and temperature if ir:
+        if self.spectrum_type == "IR":
+            self.temperature.layout.display = "none"
+            self.frequency_laser.layout.display = "none"
 
         self._plot_type.observe(self._on_plot_type_change, names="value")
         self.plot_button.on_click(self._on_plot_button_clicked)
@@ -244,27 +278,70 @@ class SpectrumPlotWidget(ipw.VBox):
     def _on_plot_button_clicked(self, change):
         if self._plot_type.value == "powder":
             # Powder spectrum
-            polarized_intensities, depolarized_intensities, frequencies, labels = self.vibro.run_powder_raman_intensities(frequencies=self.frequency_laser.value, temperature=self.temperature.value)
-            total_intensities =  polarized_intensities + depolarized_intensities
-            self.frequencies , self.intensities = plot_powder(frequencies, total_intensities)
+            if self.spectrum_type == "Raman":
+                (
+                    polarized_intensities,
+                    depolarized_intensities,
+                    frequencies,
+                    labels,
+                ) = self.vibro.run_powder_raman_intensities(
+                    frequencies=self.frequency_laser.value,
+                    temperature=self.temperature.value,
+                )
+                total_intensities = polarized_intensities + depolarized_intensities
+            else:
+                (
+                    polarized_intensities,
+                    frequencies,
+                    labels,
+                ) = self.vibro.run_powder_ir_intensities()
+                total_intensities = polarized_intensities
+
+            self.frequencies, self.intensities = plot_powder(
+                frequencies, total_intensities
+            )
             self._display_figure()
-            
-            
+
         else:
             # Single crystal spectrum
-            dir_incoming, correct_syntax_incoming = self._check_inputs_correct(self.pol_incoming)
-            dir_outgoing, correct_syntax_outgoing = self._check_inputs_correct(self.pol_outgoing)
+            dir_incoming, correct_syntax_incoming = self._check_inputs_correct(
+                self.pol_incoming
+            )
+            dir_outgoing, correct_syntax_outgoing = self._check_inputs_correct(
+                self.pol_outgoing
+            )
             if not correct_syntax_incoming or not correct_syntax_outgoing:
                 self.wrong_syntax.layout.visibility = "visible"
                 return
             else:
                 self.wrong_syntax.layout.visibility = "hidden"
-                intensities, frequencies, labels = self.vibro.run_single_crystal_raman_intensities(pol_incoming=dir_incoming, pol_outgoing=dir_incoming, frequencies=self.frequency_laser.value, temperature=self.temperature.value)
-                self.frequencies , self.intensities = plot_powder(frequencies, intensities)
+                if self.spectrum_type == "Raman":
+                    (
+                        intensities,
+                        frequencies,
+                        labels,
+                    ) = self.vibro.run_single_crystal_raman_intensities(
+                        pol_incoming=dir_incoming,
+                        pol_outgoing=dir_incoming,
+                        frequencies=self.frequency_laser.value,
+                        temperature=self.temperature.value,
+                    )
+                else:
+                    (
+                        intensities,
+                        frequencies,
+                        labels,
+                    ) = self.vibro.run_single_crystal_ir_intensities(
+                        pol_incoming=dir_incoming, pol_outgoing=dir_incoming
+                    )
+
+                self.frequencies, self.intensities = plot_powder(
+                    frequencies, intensities
+                )
                 self._display_figure()
 
-    def _check_inputs_correct(self,polarization):
-        #Check if the polarization vectors are correct
+    def _check_inputs_correct(self, polarization):
+        # Check if the polarization vectors are correct
         input_text = polarization.value
         input_values = input_text.split()
         dir_values = []
@@ -282,56 +359,62 @@ class SpectrumPlotWidget(ipw.VBox):
 
         fig = go.FigureWidget(
             layout=go.Layout(
-                title=dict(text="Raman spectrum"),
+                title=dict(text=f"{self.spectrum_type} spectrum"),
                 barmode="overlay",
             )
         )
         fig.layout.xaxis.title = "Wavenumber (cm-1)"
         fig.layout.yaxis.title = "Intensity (arb. units)"
-        fig.layout.xaxis.nticks = 0 
-        fig.add_scatter(x=self.frequencies,y=self.intensities,name=f"")
+        fig.layout.xaxis.nticks = 0
+        fig.add_scatter(x=self.frequencies, y=self.intensities, name=f"")
         fig.update_layout(
             height=500,
             width=700,
             plot_bgcolor="white",
-
-
         )
         return fig
 
     def _display_figure(self):
         with self.spectrum_widget:
-                    clear_output()
-                    display(self._spectrum_widget())
-                    self.download_button.layout.visibility = "visible"
-        
-        
-  
-
-
+            clear_output()
+            display(self._spectrum_widget())
+            self.download_button.layout.visibility = "visible"
 
 
 class ActiveModesWidget(ipw.VBox):
     """Widget that display an animation (nglview) of the active modes."""
+
     def __init__(self, node, **kwargs):
         self.node = node
-        #VibriationalData 
-        self.vibro = self.node.outputs.vibronic.iraman.vibrational_data.numerical_accuracy_4 
-        #Raman active modes
-        frequencies, self.eigenvectors, self.labels = self.vibro.run_active_modes(selectrion_rules='raman')
+        vibro = self.node.outputs.vibronic.iraman.vibrational_data.numerical_accuracy_4
+
+        self.spectrum_type = (
+            "Raman" if "raman_tensors" in vibro.get_arraynames() else "IR"
+        )
+        # VibriationalData
+        self.vibro = (
+            self.node.outputs.vibronic.iraman.vibrational_data.numerical_accuracy_4
+        )
+        # Raman or IR active modes
+        frequencies, self.eigenvectors, self.labels = self.vibro.run_active_modes(
+            selectrion_rules=self.spectrum_type.lower()
+        )
         self.rounded_frequencies = [round(frequency, 3) for frequency in frequencies]
 
-        #StructureData
+        # StructureData
         self.structure_ase = self.node.inputs.structure.get_ase()
 
         self.ngl = nv.NGLWidget()
-        
-        modes_values = [f"{index + 1}: {value}" for index, value in enumerate(self.rounded_frequencies)]
+
+        modes_values = [
+            f"{index + 1}: {value}"
+            for index, value in enumerate(self.rounded_frequencies)
+        ]
         # Create Raman modes widget
         self.active_modes = ipw.Dropdown(
-            options= modes_values,
+            options=modes_values,
             value=modes_values[0],  # Default value
-            description='Select mode:',
+            description="Select mode:",
             style={"description_width": "initial"},
         )
 
@@ -347,7 +430,7 @@ class ActiveModesWidget(ipw.VBox):
             ipw.BoundedIntText(value=1, min=1, layout={"width": "40px"}),
             ipw.BoundedIntText(value=1, min=1, layout={"width": "40px"}),
         ]
-        
+
         self.supercell_selector = ipw.HBox(
             [
                 ipw.HTML(
@@ -361,21 +444,46 @@ class ActiveModesWidget(ipw.VBox):
         self.animation = ipw.Output()
         self._display_table()
         self._select_active_mode(None)
-        widget_list = [self.active_modes, self.amplitude, self._supercell[0], self._supercell[1], self._supercell[2]]
+        widget_list = [
+            self.active_modes,
+            self.amplitude,
+            self._supercell[0],
+            self._supercell[1],
+            self._supercell[2],
+        ]
         for elem in widget_list:
             elem.observe(self._select_active_mode, names="value")
-        
+
         super().__init__(
             children=[
-                ipw.HBox([ipw.VBox([ipw.HTML(value="<b> Raman Active Modes </b>"),self.modes_table]),ipw.VBox([self.active_modes, self.amplitude, self.supercell_selector, self.animation], layout={"justify_content": "center"})]),
+                ipw.HBox(
+                    [
+                        ipw.VBox(
+                            [
+                                ipw.HTML(
+                                    value=f"<b> {self.spectrum_type} Active Modes </b>"
+                                ),
+                                self.modes_table,
+                            ]
+                        ),
+                        ipw.VBox(
+                            [
+                                self.active_modes,
+                                self.amplitude,
+                                self.supercell_selector,
+                                self.animation,
+                            ],
+                            layout={"justify_content": "center"},
+                        ),
+                    ]
+                ),
             ]
-
         )
 
     def _display_table(self):
         """Display table with the active modes."""
-        #Create an HTML table with the active modes
-        table_data = [list(x) for x in zip(self.rounded_frequencies,self.labels)]
+        # Create an HTML table with the active modes
+        table_data = [list(x) for x in zip(self.rounded_frequencies, self.labels)]
         table_html = "<table>"
         table_html += "<tr><th>Frequencies (cm-1) </th><th> Label</th></tr>"
         for row in table_data:
@@ -400,7 +508,7 @@ class ActiveModesWidget(ipw.VBox):
         with self.animation:
             clear_output()
             display(self.ngl)
-    
+
     def _animation_widget(self):
         """Create animation widget."""
         # Get the index of the selected mode
@@ -422,17 +530,21 @@ class ActiveModesWidget(ipw.VBox):
                 cell=structure.cell,
                 pbc=True,
             )
-            supercell = vibro_atoms.repeat((self._supercell[0].value, self._supercell[1].value, self._supercell[2].value))
+            supercell = vibro_atoms.repeat(
+                (
+                    self._supercell[0].value,
+                    self._supercell[1].value,
+                    self._supercell[2].value,
+                )
+            )
             traj.append(supercell)
         # Create the animation widget
         self.ngl.clear()
-        self.ngl = nv.show_asetraj(traj) 
+        self.ngl = nv.show_asetraj(traj)
         self.ngl.add_unitcell()
-        #Make the animation to be set in a loop
+        # Make the animation to be set in a loop
         self.ngl._iplayer.children[0]._playing = False
         self.ngl._iplayer.children[0]._playing = True
         self.ngl._iplayer.children[0]._repeat = True
         self.ngl._set_size("400px", "400px")
         self.ngl.center()
-    
-    
--- aiidalab_qe_vibroscopy/workflows/settings.py
+++ aiidalab_qe_vibroscopy/workflows/settings.py
@@ -8,8 +8,10 @@ Authors:
 """
 import ipywidgets as ipw
 from aiida.orm import Float, Int, Str
+import traitlets as tl
 
 from aiidalab_qe.common.panel import Panel
+
 
 class Setting(Panel):
     title = "Vibrational Settings"
@@ -25,46 +27,46 @@ class Setting(Panel):
             supercell size of 2x2x2 is at convergence with respect to finite atomic displacement in the unit cell.
             </div>"""
         )
-        
+
         self.polar_help = ipw.HTML(
             """<div style="line-height: 140%; padding-top: 5px; padding-bottom: 5px">
-            If the material is polar, more 
+            If the material is polar, more
             accurate phonon properties interpolation is performed.
             </div>"""
         )
-        
+
         self.workchain_protocol = ipw.ToggleButtons(
             options=["fast", "moderate", "precise"],
             value="moderate",
         )
-        
-        #I want to be able to select more than only one... this has to change at the PhononWorkChain level.
+
+        # I want to be able to select more than only one... this has to change at the PhononWorkChain level.
         self.phonon_property_ = ipw.Dropdown(
             options=[
-                ["band structure","BANDS"], 
-                ["density of states (DOS)","DOS"], 
-                ["thermal properties","THERMODYNAMIC"],
-                ["force constants","NONE"],
-                ["none","none"]
+                ["band structure", "BANDS"],
+                ["density of states (DOS)", "DOS"],
+                ["thermal properties", "THERMODYNAMIC"],
+                ["force constants", "NONE"],
+                ["none", "none"],
             ],
             value="none",
             description="Phonon property:",
             disabled=False,
             style={"description_width": "initial"},
         )
-        
+
         self.dielectric_property_ = ipw.Dropdown(
             options=[
-                ["dielectric tensor","dielectric"], 
-                ["infrared","ir"], 
-                ["raman",'raman'], 
-                ['born-charges',"born-charges"], 
-                #'nac', 
-                #'bec', 
+                ["dielectric tensor", "dielectric"],
+                ["infrared", "ir"],
+                ["raman", "raman"],
+                ["born-charges", "born-charges"],
+                #'nac',
+                #'bec',
                 #'susceptibility-derivative',
                 #'non-linear-susceptibility',
-                ['none','none'],
-                ],
+                ["none", "none"],
+            ],
             value="none",
             description="Dielectric property:",
             disabled=False,
@@ -72,38 +74,56 @@ class Setting(Panel):
         )
 
         self.spectrum_ = ipw.ToggleButtons(
-            options=[("Off","off"),("Infrared", "ir"), ("Raman", "raman")],
+            options=[("Off", "off"), ("Infrared", "ir"), ("Raman", "raman")],
             value="off",
             style={"description_width": "initial"},
         )
-        
-        #start Supercell
-        self.supercell=[2,2,2]
+
+        self.spectrum_.observe(self._onclick_spectrum_choice, "value")
+
+        # start Supercell
+        self.supercell = [2, 2, 2]
+
         def change_supercell(_=None):
             self.supercell = [
                 self._sc_x.value,
                 self._sc_y.value,
                 self._sc_z.value,
             ]
-            
-        for elem in ["x","y","z"]:
-            setattr(self,"_sc_"+elem,ipw.BoundedIntText(value=2, min=1, layout={"width": "40px"},disabled=False))
-        
-        for elem in [self._sc_x,self._sc_y,self._sc_z]:
+
+        for elem in ["x", "y", "z"]:
+            setattr(
+                self,
+                "_sc_" + elem,
+                ipw.BoundedIntText(
+                    value=2, min=1, layout={"width": "40px"}, disabled=False
+                ),
+            )
+
+        for elem in [self._sc_x, self._sc_y, self._sc_z]:
             elem.observe(change_supercell, names="value")
-            
+
         self.supercell_selector = ipw.HBox(
-            children=[ipw.HTML(description="Supercell size:",style={"description_width": "initial"})] + [
-                self._sc_x,self._sc_y,self._sc_z,], 
+            children=[
+                ipw.HTML(
+                    description="Supercell size:",
+                    style={"description_width": "initial"},
+                )
+            ]
+            + [
+                self._sc_x,
+                self._sc_y,
+                self._sc_z,
+            ],
         )
-                    
+
         self.supercell_widget = ipw.HBox(
             [self.supercell_selector],
             layout=ipw.Layout(justify_content="flex-start"),
         )
-        #end Supercell.
-         
-        #to trigger Dielectric property = Raman... FOR POLAR MATERIALS. 
+        # end Supercell.
+
+        # to trigger Dielectric property = Raman... FOR POLAR MATERIALS.
         self.material_is_polar_ = ipw.ToggleButtons(
             options=[("Off", "off"), ("On", "on")],
             value="off",
@@ -126,7 +146,7 @@ class Setting(Panel):
                 children=[
                     self.phonon_property_,
                     self.supercell_widget,
-                    ],
+                ],
                 layout=ipw.Layout(justify_content="flex-start"),
             ),
             self.dielectric_property_,
@@ -143,28 +163,34 @@ class Setting(Panel):
         ]
         super().__init__(**kwargs)
 
+    def _onclick_spectrum_choice(self, change):
+        if change["new"] == "raman":
+            self.dielectric_property_.value = "raman"
+        elif change["new"] == "ir":
+            self.dielectric_property_.value = "ir"
+
     def get_panel_value(self):
         """Return a dictionary with the input parameters for the plugin."""
         return {
-                "phonon_property": self.phonon_property_.value,
-                "dielectric_property": self.dielectric_property_.value,
-                "material_is_polar": self.material_is_polar_.value,
-                "supercell_selector": self.supercell,
-                "spectrum": self.spectrum_.value,
-                }
+            "phonon_property": self.phonon_property_.value,
+            "dielectric_property": self.dielectric_property_.value,
+            "material_is_polar": self.material_is_polar_.value,
+            "supercell_selector": self.supercell,
+            "spectrum": self.spectrum_.value,
+        }
 
     def load_panel_value(self, input_dict):
         """Load a dictionary with the input parameters for the plugin."""
-        self.phonon_property_.value = input_dict.get("phonon_property","none")
+        self.phonon_property_.value = input_dict.get("phonon_property", "none")
         self.dielectric_property_.value = input_dict.get("dielectric_property", "none")
         self.material_is_polar_.value = input_dict.get("material_is_polar", "off")
         self.spectrum_.value = input_dict.get("spectrum", "off")
-        self.supercell = input_dict.get("supercell_selector", [2,2,2])
-        
+        self.supercell = input_dict.get("supercell_selector", [2, 2, 2])
+
     def reset(self):
         """Reset the panel"""
         self.phonon_property_.value = "none"
         self.dielectric_property_.value = "none"
         self.material_is_polar_.value = "off"
         self.spectrum_.value = "off"
-        self.supercell = [2,2,2]
+        self.supercell = [2, 2, 2]
