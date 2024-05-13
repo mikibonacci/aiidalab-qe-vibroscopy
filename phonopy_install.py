f4bea8eb1e3f753b00375f66daac6adcffbb771d
Xing Wang <xingwang1991@gmail.com>
2024-05-13 11:53:43 +0200

    find phonopy path automatically (#56)

--- phonopy_install.py
+++ phonopy_install.py
@@ -2,6 +2,7 @@ from aiida.common.exceptions import NotExistent
 import subprocess
 from aiida.orm import load_code
 from aiida import load_profile
+import shutil
 
 
 def install_phonopy():
@@ -9,6 +10,8 @@ def install_phonopy():
     try:
         load_code("phonopy@localhost")
     except NotExistent:
+        # Use shutil.which to find the path of the phonopy executable
+        phonopy_path = shutil.which("phonopy")
         # Construct the command as a list of arguments
         command = [
             "verdi",
@@ -23,7 +26,7 @@ def install_phonopy():
             "--computer",
             "localhost",
             "--filepath-executable",
-            "/opt/conda/bin/phonopy",
+            phonopy_path,
         ]
 
         # Use subprocess.run to run the command
